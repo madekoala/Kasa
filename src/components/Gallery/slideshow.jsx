deaf31b0a1ed62ba55aa81f1402be1c7390d3455
Cresson Jeremy <jrmcresson@gmail.com>
2024-10-18 18:14:36 +0200

    update Header + modif slide show + ajout fichier png

--- src/components/Gallery/slideshow.jsx
+++ src/components/Gallery/slideshow.jsx
@@ -3,49 +3,54 @@ import React from "react";
 // Import du hook useState de React
 import { useState } from "react";
 // import des fleches
+import { ReactComponent as ArrowLeft } from "../Gallery/arrowleft.svg";
+import { ReactComponent as ArrowRight } from "../Gallery/arrowright.svg";
 
 // Composant Slideshow : affiche un carrousel d'images
 function Slideshow(props) {
-    // Slide actuelle
-    const [currentSlide, setCurrentSlide] = useState(0);
-    // Calcule longueur totale de 'pictures' en extrayant la propriété 'pictures' de chaque élément du tableau 'props.pictures'
-    const arrayLength = props.pictures.flatMap((el) => el.pictures).length;
-   
-    // Fonction pour passer à la slide précédente
-    function prevSlide() {
-      let newSlide = currentSlide === 0 ? arrayLength - 1 : currentSlide - 1;
-      setCurrentSlide(newSlide);
-       
-    }
-  
-    // Fonction pour passer à la slide suivante
-    function nextSlide() {
-      let newSlide = currentSlide === arrayLength - 1 ? 0 : currentSlide + 1;
-      setCurrentSlide(newSlide);
-      
-    }
-  
-    // Accède directement à l'élément correspondant dans le tableau props.pictures
-    const currentPicture = props.pictures[currentSlide];
-  
-    return (
-      // Création de la section qui accueillera les slides
-      <section className="slideshow_container">
-        <div className={arrayLength <= 1 ? "arrow_hidden slideshow_navigation" : "slideshow_navigation"}>
-          <ArrowLeft onClick={() => prevSlide()} />
-          <ArrowRight onClick={() => nextSlide()} />
-        </div>
-        <img
-          src={currentPicture}
-          alt=""
-          className="slideshow_img"
-        />
-        <div className={arrayLength <= 1 ? "arrow_hidden slideshow_text" : "slideshow_text"}>
-          {currentSlide + 1}/{arrayLength}
-        </div>
-      </section>
-    );
+  // Slide actuelle
+  const [currentSlide, setCurrentSlide] = useState(0);
+  // Calcule longueur totale de 'pictures' en extrayant la propriété 'pictures' de chaque élément du tableau 'props.pictures'
+  const arrayLength = props.pictures.flatMap((el) => el.pictures).length;
+
+  // Fonction pour passer à la slide précédente
+  function prevSlide() {
+    let newSlide = currentSlide === 0 ? arrayLength - 1 : currentSlide - 1;
+    setCurrentSlide(newSlide);
   }
-  
-  
-  export default Slideshow;
+
+  // Fonction pour passer à la slide suivante
+  function nextSlide() {
+    let newSlide = currentSlide === arrayLength - 1 ? 0 : currentSlide + 1;
+    setCurrentSlide(newSlide);
+  }
+
+  // Accède directement à l'élément correspondant dans le tableau props.pictures
+  const currentPicture = props.pictures[currentSlide];
+
+  return (
+    // Création de la section qui accueillera les slides
+    <section className="slideshow_container">
+      <div
+        className={
+          arrayLength <= 1
+            ? "arrow_hidden slideshow_navigation"
+            : "slideshow_navigation"
+        }
+      >
+        <ArrowLeft onClick={() => prevSlide()} />
+        <ArrowRight onClick={() => nextSlide()} />
+      </div>
+      <img src={currentPicture} alt="" className="slideshow_img" />
+      <div
+        className={
+          arrayLength <= 1 ? "arrow_hidden slideshow_text" : "slideshow_text"
+        }
+      >
+        {currentSlide + 1}/{arrayLength}
+      </div>
+    </section>
+  );
+}
+
+export default Slideshow;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,35 @@
+// Import de React depuis la bibliothèque React
 import React from "react";
-import { Link } from "react-router-dom";
+// Import de NavLink depuis la bibliothèque react-router-dom
+import { NavLink } from "react-router-dom";
 
+// Composant Header : représente l'en-tête de la page
 function Header() {
+  // Rendu du composant
   return (
     <header>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Accueil</Link>
-          </li>
-          <li>
-            <Link to="/test">Test</Link>
-          </li>
+      <nav className="navigation">
+        <div className="nav_logo">
+          <NavLink to="/">
+            <img src={process.env.PUBLIC_URL + "/LOGO.png"} alt="logo kasa" />
+          </NavLink>
+        </div>
+        <ul className="nav_link">
+          <NavLink
+            to="/"
+            className={(nav) => (nav.isActive ? "nav_active" : "")}
+            end
+          >
+            <li>Accueil</li>
+          </NavLink>
+
+          <NavLink
+            to="/about"
+            className={(nav) => (nav.isActive ? "nav_active" : "")}
+            end
+          >
+            <li>A Propos</li>
+          </NavLink>
         </ul>
       </nav>
     </header>
